70842807920b4837f89012dd6bf91275a7be301d
Success Go <successgdc@gmail.com>
2021-11-27 20:38:54 +0800

    Init console application.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CertChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
